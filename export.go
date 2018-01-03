8070d364cb4fb7871b099f614b9c296866d6f037
Abhishek Soni <abhishek.rocks26@gmail.com>
2018-01-03 13:43:23 +0530

    Code for basic methods

---
+++ export.go
@@ -0,0 +1,17 @@
+package taskrunner
+
+import "context"
+
+func NewTaskManager() *TaskManager {
+	return &TaskManager{
+		ledger:make(map[string]*Task),
+	}
+}
+
+func newMetric(cancelFunc context.CancelFunc, fn runnable) *Task {
+	return &Task{
+		cancelFunc:cancelFunc,
+		meta:make(map[string]interface{}),
+		source:fn,
+	}
+}
---
+++ task_routine.go
@@ -0,0 +1,120 @@
+package taskrunner
+
+import (
+	"sync"
+	"context"
+	"github.com/pkg/errors"
+	"github.com/satori/uuid"
+)
+
+var (
+	ErrorKeyAlreadyExistInMeta = errors.New("key already exist in meta")
+	ErrorKeyDidNotExistInMeta = errors.New("key is not present in meta")
+)
+
+type runnable func(ctx context.Context, meta *Task)
+
+
+type Task struct {
+	sync.RWMutex
+	cancelFunc context.CancelFunc
+	source runnable
+	meta map[string]interface{}
+}
+
+func (m *Task) AddMeta(key string, val interface{}) error{
+	m.Lock()
+	defer m.Unlock()
+	if _, ok :=m.meta[key]; ok {
+		return ErrorKeyAlreadyExistInMeta
+	}
+	m.meta[key] = val
+
+	return nil
+}
+
+func (m *Task) RemoveMeta(key string) error{
+	m.Lock()
+	defer m.Unlock()
+	if _, ok :=m.meta[key]; !ok {
+		return ErrorKeyDidNotExistInMeta
+	}
+	delete(m.meta, key)
+	return nil
+}
+
+func (m *Task) Cancel(){
+	m.Lock()
+	defer m.Unlock()
+	m.cancelFunc()
+}
+
+type TaskManager struct {
+	sync.RWMutex
+	count int
+	open bool
+	ledger map[string]*Task
+}
+
+func (tm *TaskManager) addTask(cancelFunc context.CancelFunc, taskid string, fn runnable) *Task {
+	tm.Lock()
+	defer tm.Unlock()
+	tm.count++
+	tm.ledger[taskid] = newMetric(cancelFunc, fn)
+	return tm.ledger[taskid]
+}
+
+func (tm *TaskManager) done(taskid string) {
+	tm.Lock()
+	defer tm.Unlock()
+	tm.count--
+	delete(tm.ledger, taskid)
+}
+
+func (tm *TaskManager) GO(fn runnable) string {
+	taskid := uuid.NewV4().String()
+	go func() {
+		ctx,cancel := context.WithCancel(context.Background())
+		fn(ctx, tm.addTask(cancel, taskid, fn))
+		tm.done(taskid)
+	}()
+	return taskid
+}
+
+func (tm *TaskManager) IsOpen() bool {
+	tm.RLock()
+	defer tm.RUnlock()
+	return tm.open
+}
+
+func (tm *TaskManager) FindFromMeta(key string) *Task {
+	tm.RLock()
+	defer tm.RUnlock()
+
+	for lkey, led := range tm.ledger{
+		led.RLock()
+		if _, ok :=led.meta[key]; ok {
+			led.RUnlock()
+			return tm.ledger[lkey]
+		}
+		led.RUnlock()
+	}
+	return nil
+}
+
+
+func (tm *TaskManager) CancelTaskFromMetaKey(key string) {
+	if task := tm.FindFromMeta(key); task != nil {
+		task.Cancel()
+	}
+}
+
+
+func (tm *TaskManager) RestartTaskFromMetaKey(key string) {
+	if task := tm.FindFromMeta(key); task != nil {
+		source := task.source
+		task.Cancel()
+		tm.GO(source)
+	}
+}
+
